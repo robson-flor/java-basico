36ae817c5525456b522120c2ec1e217b10a838ac
robson-flor <robsonbuziosbr@outlook.com>
2022-10-28 17:37:49 -0300

    declaração dos tipos primitivos

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/Tipos_Primitivos.java
@@ -0,0 +1,22 @@
+import java.util.function.ToIntBiFunction;
+
+public class Tipos_Primitivos {
+  public static void main(String[] args) {
+    byte tipoByte = 127;
+    short tipoShort = 32767;
+    char tipoChar = 'C';
+    float tipoFloat = 2.6f;
+    double tipoDouble = 3.59;
+    int tipoInt = 2147483647;
+    long tipoLong = 9223372036854775807L;
+    boolean tipoBooleano = true;
+    System.out.println("Valor do tipoByte = " + tipoByte);
+    System.out.println("valor do tipoShort = " + tipoShort);
+    System.out.println("Valor do tipoChar = " + tipoChar);
+    System.out.println("Valor do tipoFloat = " + tipoFloat);
+    System.out.println("Valor do tipoDouble = " + tipoFloat);
+    System.out.println("Valor do tipo tipoInt" + tipoInt);
+    System.out.println("Valor do tipoLong " + tipoLong);
+    System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+  }
+}
