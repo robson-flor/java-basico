ee0577360108b458ff2868f8af5034118bf0e3f1
robson-flor <robsonbuziosbr@outlook.com>
2022-10-29 17:29:15 -0300

    acessando um método do objeto.

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/acessaMetodoObj.java
@@ -0,0 +1,13 @@
+/**
+ * acessaMetodoObj
+ */
+public class acessaMetodoObj {
+
+  public void imprime() {
+    System.out.println("Bem vindo ao Java!");
+  }
+  public static void main(String[] args) {
+    acessaMetodoObj acessa = new acessaMetodoObj();
+    acessa.imprime();
+  }
+}
