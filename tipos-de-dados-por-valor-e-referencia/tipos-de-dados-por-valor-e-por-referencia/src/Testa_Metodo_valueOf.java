87e61b3574914fff45fa08420a5e3e8b4757c497
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 23:15:58 -0300

    String.valueOf

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/Testa_Metodo_valueOf.java
@@ -0,0 +1,10 @@
+public class Testa_Metodo_valueOf {
+  public static void main(String[] args) {
+    double numero = 102939939.939;
+    boolean booleano = true;
+
+    System.out.println("Retorna Valor : " + String.valueOf(numero));
+    System.out.println("Retorna Valor : " + String.valueOf(booleano));
+    
+  }
+}
