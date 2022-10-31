c61435916ac16a5a9ef0b9e7b91cbec666d46f4c
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 21:49:19 -0300

    equals

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/iguais.java
@@ -0,0 +1,12 @@
+public class iguais {
+  public static void main(String[] args) {
+    String nome1 = "Carlos";
+    String nome2 = "Carla";
+
+    if (nome1.equals(nome2)) {
+      System.out.println("Os nome sao iguais!");
+    } else {
+      System.out.println("Os nomes sao diferentes!");
+    }
+  }
+}
