b22b0981923213eb046678e608f16e385e2a062d
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 21:58:02 -0300

    compareTo

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/comparaTo.java
@@ -0,0 +1,9 @@
+public class comparaTo {
+  public static void main(String[] args) {
+    String nome1 = "Carlos";
+    String nome2 = "Carla";
+
+    System.out.println("nome2.compareTo(nome1) = " +nome2.compareTo(nome1));
+    System.out.println("nome1.compareTo(nome2) = " +nome1.compareTo(nome2));
+  }
+}
