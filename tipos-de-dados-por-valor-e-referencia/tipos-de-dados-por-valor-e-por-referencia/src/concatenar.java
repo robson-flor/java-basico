e0d0b93900225fe55730a374dead6331a288d1df
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 22:45:33 -0300

    utilizando o método concat para concatenar strings

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/concatenar.java
@@ -0,0 +1,11 @@
+public class concatenar {
+  public static void main(String[] args) {
+    String nome = "Robson";
+    String sobrenome = " Flor";
+    
+
+    String nomeCompleto = nome.concat(sobrenome);
+
+    System.out.println(nomeCompleto);
+  }
+}
