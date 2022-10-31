707148a7647d7d8ab15dc25eba2ef88019603100
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 21:50:14 -0300

    exemplo string

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/string.java
@@ -0,0 +1,10 @@
+public class string {
+  public static void main(String[] args) {
+    "qualquer coisa dentro de aspas".length();
+    String texto;
+    texto = "123";
+    System.out.println(texto);
+  
+    
+  }
+}
