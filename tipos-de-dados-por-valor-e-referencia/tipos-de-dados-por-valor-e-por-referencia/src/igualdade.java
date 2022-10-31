862f3e4aa5415b21335ed977558956143aef3933
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 21:49:54 -0300

    igualdade

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/igualdade.java
@@ -0,0 +1,11 @@
+public class igualdade {
+
+  public static void main(String[] args) {
+    String nome = "Arthur";
+    String apelido = nome;
+
+    if(nome == apelido) {
+      System.out.println("Nome e apelido sao iguais");
+    }
+  }
+}
