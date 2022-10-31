a68f0acf68e963fd86492997dd06ccfc9559ff85
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 23:32:34 -0300

    Length

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/comprimento.java
@@ -0,0 +1,10 @@
+/**
+ * comprimento
+ */
+public class comprimento {
+
+  public static void main(String[] args) {
+    String nomeCurso = "Java";
+    System.out.printf("\nTamanho da variavel nomeCurso: %d", nomeCurso.length());
+  }
+}
