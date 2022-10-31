86bd60eda0d99b2f891b68ca2aa617710c7d509f
robson-flor <robsonbuziosbr@outlook.com>
2022-10-30 23:40:42 -0300

    charAt

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/characAt.java
@@ -0,0 +1,9 @@
+public class characAt {
+  public static void main(String[] args) {
+    String carac = "JAVA";
+
+    if (carac.charAt(1) == 'A') {
+      System.out.println("O caracter A esta na posicao 1");
+    }
+  }
+}
