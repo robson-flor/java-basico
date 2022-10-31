de3316486f32b13cbdc8034895819fb95486a196
robson-flor <robsonbuziosbr@outlook.com>
2022-10-31 19:26:01 -0300

    método replace

---
+++ tipos-de-dados-por-valor-e-referencia/tipos-de-dados-por-valor-e-por-referencia/src/replacee.java
@@ -0,0 +1,7 @@
+public class replacee {
+  public static void main(String[] args) {
+    String nome = "mesquita";
+    String nomeAlterado = nome.replace('e', 'o');
+    System.out.println(nomeAlterado);
+  }
+}
